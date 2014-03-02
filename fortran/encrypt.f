8b288ac0a54c9e13af7d09781866b43fc8c4a8a5
Sovietaced <Sovietaced@gmail.com>
2014-03-01 23:37:34 -0800

    working encrypt

--- fortran/encrypt.f
+++ fortran/encrypt.f
@@ -1,5 +1,4 @@
-function to_upper(strIn) result(strOut)
-  
+function to_upper(strIn) result(strOut)  
   implicit none
   
   character(len=*), intent(in) :: strIn
@@ -16,14 +15,41 @@ function to_upper(strIn) result(strOut)
   end do
 end function to_upper
 
+function shift(strIn, numShift) result(strOut)
+  implicit none
+
+  character(len=*), intent(in) :: strIn
+  character(len=len(strIn)) :: strOut
+  integer :: i,j,low,high,numShift
+   
+  low = iachar("A")
+  high = iachar("Z")
+  
+  do i = 1, len(strIn)
+    j = iachar(strIn(i:i))
+    ! Only evaluate valid characters
+    if (j /= 32) then 
+      if (j + numShift >= high) then
+        strOut(i:i) = achar(low+(high-j))
+      else
+        strOut(i:i) = achar(j+numShift)
+      end if
+    else
+      strOut(i:i) = strIn(i:i)
+    end if
+  end do
+end function shift
+
 program main
-  character(100) :: input, to_upper
+  character(100) :: input, to_upper, shift
   integer :: numShift
   print *, "Enter a string"
   READ(*,*) input
   input = to_upper(input)
+  print *, input
   print *, "Enter an integer"
   READ(*,*) numShift
+  input = shift(input, numShift)
   print *, input
 end program main
 
