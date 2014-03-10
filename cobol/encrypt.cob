79209657c9a1cd86aad05246891dc3b2f0ce021e
Sovietaced <Sovietaced@gmail.com>
2014-03-10 03:26:41 -0700

    accepting input

--- cobol/encrypt.cob
+++ cobol/encrypt.cob
@@ -1,5 +1,20 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. HELLO-WORLD.
-PROCEDURE DIVISION.
-    DISPLAY 'Hello world!'.
-    STOP RUN.
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. caesar.
+        
+        ENVIRONMENT DIVISION.
+        
+        DATA DIVISION.
+        
+        WORKING-STORAGE SECTION.
+        01 inputString PIC x(99).
+        01 numShift PIC 99.
+	
+        PROCEDURE DIVISION.
+		DISPLAY "Enter a string: " NO ADVANCING
+    		ACCEPT inputString
+		DISPLAY "Enter an integer: " NO ADVANCING
+		ACCEPT numShift    
+		STOP RUN.
+    	
+	encrypt.
+	
