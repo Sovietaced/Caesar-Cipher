519f62eb14c63120ba62a271775ff627afb50363
Sovietaced <Sovietaced@gmail.com>
2014-04-27 06:06:49 -0400

    to upper

--- erlang/encrypt.erl
+++ erlang/encrypt.erl
@@ -1,4 +1,4 @@
--module(encrypt).    % This is the file 'fact.erl', the module and the filename must match
--export([encrypt/0]). % This exports the function 'fac' of arity 1 (1 parameter, no type, no name)
+-module(encrypt).   
+-export([encrypt/2]). 
  
-encrypt() -> io:get_line("Prompt> ").
+encrypt(Input, Shift) -> io:fwrite("~s", [string:to_upper(Input)]).
